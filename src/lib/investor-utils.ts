9a168c2fdaa7bd4b97f1b866bc53b030ba12395d
Visar Curraj <visar@MBP-von-Visar.fritz.box>
2025-05-31 01:33:38 +0200

    feat: add investor-utils stub

---
+++ src/lib/investor-utils.ts
@@ -0,0 +1,9 @@
+// src/lib/investor-utils.ts
+/**
+ * Hier kommen deine Investor-Utility-Funktionen rein.
+ * Für den Build reicht erstmal ein Dummy.
+ */
+export async function fetchInvestorUpdates() {
+    // TODO: Implementiere später (oder lass es leer)
+    return []
+  }
