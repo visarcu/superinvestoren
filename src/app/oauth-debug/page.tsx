dd068dde77718557752a2d1c01d1ffbf20f57236
Visar Curraj <visar@MBP-von-Visar.fritz.box>
2025-06-22 09:39:22 +0200

    NEW FEATURES: AI, financials, design improvement and much more

--- src/app/oauth-debug/page.tsx
+++
@@ -1,9 +0,0 @@
-import GoogleOAuthDebug from '@/components/GoogleOAuthDebug'
-
-export default function OAuthDebugPage() {
-  return (
-    <div className="min-h-screen bg-gray-900 py-8">
-      <GoogleOAuthDebug />
-    </div>
-  )
-}
